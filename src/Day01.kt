a860f453927b80f51923934e034db3869e2083f3
Marcus Nordberg <nordbergaren93@gmail.com>
2022-12-03 12:44:18 +0100

    Finish Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
+import java.lang.Integer.parseInt
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSoFar = Int.MIN_VALUE
+        var currentCounter = 0
+        input.forEach { s ->
+            if (s.isEmpty()) {
+                maxSoFar = maxSoFar.coerceAtLeast(currentCounter)
+                currentCounter = 0
+            } else {
+                currentCounter += s.toInt()
+            }
+        }
+        return maxSoFar
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val biggest = mutableListOf<Int>()
+
+        var maxSoFar = Int.MIN_VALUE
+        var currentCounter = 0
+        input.forEach { s ->
+            if (s.isEmpty()) {
+                biggest += currentCounter
+                currentCounter = 0
+            } else {
+                currentCounter += s.toInt()
+            }
+        }
+
+        biggest.sortDescending()
+        return biggest.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
